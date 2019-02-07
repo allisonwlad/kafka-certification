c70d50b3a60bdb8d6a373399800da1dae572f1c2
allison <allison@allison-pc>
2019-02-06 22:03:27 -0200

    agora sim

--- kafka-beginners-course/src/main/java/com/github/allisonwlad/kafka/kafka_beginners_course/KafkaProducerMain.java
+++ kafka-beginners-course/src/main/java/com/github/allisonwlad/kafka/kafka_beginners_course/KafkaProducerMain.java
@@ -2,6 +2,6 @@ package com.github.allisonwlad.kafka.kafka_beginners_course;
 
 public class KafkaProducerMain {
 	public static void main(String[] args) {
-		System.out.println("teste");
+		System.out.println("teste allison");
 	}
 }
